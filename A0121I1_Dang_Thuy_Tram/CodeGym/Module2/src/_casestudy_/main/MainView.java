20adabab131c0b78d362df854a7a09c910dc6429
tramdangthuy <tramdangthuy3011@gmail.com>
2021-06-21 18:05:33 +0700

    update casestudy_ver1

--- A0121I1_Dang_Thuy_Tram/CodeGym/Module2/src/_casestudy_/main/MainView.java
+++ A0121I1_Dang_Thuy_Tram/CodeGym/Module2/src/_casestudy_/main/MainView.java
@@ -21,11 +21,12 @@ public class MainView {
     public static final String VILLA_CODE = "SVVL";
     public static final String HOUSE_CODE = "SVHO";
     public static final String ROOM_CODE = "SVRO";
-    public static final String COMMA = ",";
+
     public static List<Services> villaList = new ArrayList<>();
     public static List<Services> houseList = new ArrayList<>();
     public static List<Services> roomList = new ArrayList<>();
     public static List<Customer> customerList = new ArrayList<>();
+
     public static LinkedHashMap<String, Employee> employeeHashMap = new LinkedHashMap<>();
     public static List<Customer> bookingList = new ArrayList<>();
     public static Queue<Customer> waitingList = new LinkedList<>();
@@ -33,23 +34,39 @@ public class MainView {
     public static Profile profileCarbinet = new Profile();
     public static int numTicket = 0;
     public static void main(String[] args) {
-        //Read service list data from CSV file
-        villaList = FileCSV.readServiceListFromCSV(VILLA_LINK);
-        houseList = FileCSV.readServiceListFromCSV(HOUSE_LINK);
-        roomList = FileCSV.readServiceListFromCSV(ROOM_LINK);
-        //read customerList data from CSV file.
-        customerList = FileCSV.readCustomerListFromCSV(CUSTOMER_FILE_PATH, VILLA_CODE, villaList);
-        customerList.addAll(FileCSV.readCustomerListFromCSV(CUSTOMER_FILE_PATH, HOUSE_CODE, houseList));
-        customerList.addAll(FileCSV.readCustomerListFromCSV(CUSTOMER_FILE_PATH, ROOM_CODE, roomList));
-        customerList.addAll(FileCSV.readCustomerListFromCSV(CUSTOMER_FILE_PATH, "null", null));
-        //Read Employee data from CSV file
-        employeeHashMap = FileCSV.readEmployeeFromCSV(EMPLOYEE_FILE_PATH);
+        readServices();
+        readCustomer();
+        readEmployee();
+        getListCustomer();
+        displayMainMenu();
+    }
+
+    private static void getListCustomer() {
         //list all customers who have booked service != null to bookingList
         for (Customer customer : customerList){
             if (customer.getServices() != null){
                 bookingList.add(customer);
             }
         }
-        displayMainMenu();
+    }
+
+    private static void readEmployee() {
+        //Read Employee data from CSV file
+        employeeHashMap = FileCSV.readEmployeeFromCSV(EMPLOYEE_FILE_PATH);
+    }
+
+    private static void readCustomer() {
+        //read customerList data from CSV file.
+        customerList = FileCSV.readCustomerListFromCSV(CUSTOMER_FILE_PATH, VILLA_CODE, villaList);
+        customerList.addAll(FileCSV.readCustomerListFromCSV(CUSTOMER_FILE_PATH, HOUSE_CODE, houseList));
+        customerList.addAll(FileCSV.readCustomerListFromCSV(CUSTOMER_FILE_PATH, ROOM_CODE, roomList));
+        customerList.addAll(FileCSV.readCustomerListFromCSV(CUSTOMER_FILE_PATH, "null", null));
+    }
+
+    private static void readServices() {
+        //Read service list data from CSV file
+        villaList = FileCSV.readServiceListFromCSV(VILLA_LINK);
+        houseList = FileCSV.readServiceListFromCSV(HOUSE_LINK);
+        roomList = FileCSV.readServiceListFromCSV(ROOM_LINK);
     }
 }
