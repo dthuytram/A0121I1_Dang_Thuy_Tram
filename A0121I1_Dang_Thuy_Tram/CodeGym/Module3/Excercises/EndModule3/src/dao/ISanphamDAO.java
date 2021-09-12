bc78efb808fa18a8e629657d7f4e27e8069fa40c
dtt3020 <69202479+dthuytram@users.noreply.github.com>
2021-09-12 11:43:44 +0700

    Update ISanphamDAO.java

--- A0121I1_Dang_Thuy_Tram/CodeGym/Module3/Excercises/EndModule3/src/dao/ISanphamDAO.java
+++ A0121I1_Dang_Thuy_Tram/CodeGym/Module3/Excercises/EndModule3/src/dao/ISanphamDAO.java
@@ -1,4 +1,18 @@
 package dao;
 
+import model.Sanpham;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ISanphamDAO {
+    void saveSanPham(Sanpham sanpham) throws SQLException;
+
+    Sanpham getSanPhamById(String id) throws SQLException;
+
+    List<Sanpham> getAllSanPham() throws SQLException;
+
+    boolean deleteSanPham(String id) throws SQLException;
+
+    boolean updateSanPham(Sanpham sanpham) throws SQLException;
 }
