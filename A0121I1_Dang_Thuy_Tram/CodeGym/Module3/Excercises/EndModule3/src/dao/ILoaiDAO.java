ff649a95c6d2f7a5fc6cc09ee28b5222d154e01f
dtt3020 <69202479+dthuytram@users.noreply.github.com>
2021-09-12 11:43:26 +0700

    Update ILoaiDAO.java

--- A0121I1_Dang_Thuy_Tram/CodeGym/Module3/Excercises/EndModule3/src/dao/ILoaiDAO.java
+++ A0121I1_Dang_Thuy_Tram/CodeGym/Module3/Excercises/EndModule3/src/dao/ILoaiDAO.java
@@ -1,4 +1,19 @@
 package dao;
 
-public class ILoaiDAO {
+import model.Loai;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ILoaiDAO {
+
+    void saveLoai(Loai loai) throws SQLException;
+
+    Loai getLoaiById(String id) throws SQLException;
+
+    List<Loai> getAllLoai() throws SQLException;
+
+    boolean deleteLoai(String id) throws SQLException;
+
+    boolean updateLoai(Loai loai) throws SQLException;
 }
