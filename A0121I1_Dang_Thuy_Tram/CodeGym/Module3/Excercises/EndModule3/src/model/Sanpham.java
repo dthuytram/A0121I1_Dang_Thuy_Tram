9e16731c27c690f2eb81adc93f2bb8a4ec665169
dtt3020 <69202479+dthuytram@users.noreply.github.com>
2021-09-12 11:46:09 +0700

    Update Sanpham.java

--- A0121I1_Dang_Thuy_Tram/CodeGym/Module3/Excercises/EndModule3/src/model/Sanpham.java
+++ A0121I1_Dang_Thuy_Tram/CodeGym/Module3/Excercises/EndModule3/src/model/Sanpham.java
@@ -1,4 +1,77 @@
 package model;
 
 public class Sanpham {
+    private String id_sanpham;
+    private String ten_sanpham;
+    private float gia;
+    private int soluong;
+    private String mausac;
+    private String mota;
+    private String id_loai;
+
+    public Sanpham(String  id_sanpham, String ten_sanpham, float gia, int soluong, String mausac, String mota, String id_loai) {
+        this.id_sanpham = id_sanpham;
+        this.ten_sanpham = ten_sanpham;
+        this.gia = gia;
+        this.soluong = soluong;
+        this.mausac = mausac;
+        this.mota = mota;
+        this.id_loai = id_loai;
+    }
+
+    public String getId_sanpham() {
+        return id_sanpham;
+    }
+
+    public void setId_sanpham(String id_sanpham) {
+        this.id_sanpham = id_sanpham;
+    }
+
+    public String getTen_sanpham() {
+        return ten_sanpham;
+    }
+
+    public void setTen_sanpham(String ten_sanpham) {
+        this.ten_sanpham = ten_sanpham;
+    }
+
+    public float getGia() {
+        return gia;
+    }
+
+    public void setGia(float gia) {
+        this.gia = gia;
+    }
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+
+    public String getMausac() {
+        return mausac;
+    }
+
+    public void setMausac(String mausac) {
+        this.mausac = mausac;
+    }
+
+    public String getMota() {
+        return mota;
+    }
+
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+
+    public String getId_loai() {
+        return id_loai;
+    }
+
+    public void setId_loai(String id_loai) {
+        this.id_loai = id_loai;
+    }
 }
